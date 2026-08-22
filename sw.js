318a2baf82d734b2f625ea32297fa9e354a5aff3
Guido Avogadro <90397521+guidoa87@users.noreply.github.com>
2026-08-21 22:09:11 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,8 @@
-const CACHE="personal-finances-v1";
-const APP_SHELL=["./","./index.html","./manifest.json","./icons/icon-180.png","./icons/icon-512.png"];
-self.addEventListener("install",event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)));
-  self.skipWaiting();
-});
-self.addEventListener("activate",event=>{
-  event.waitUntil(self.clients.claim());
-});
+const CACHE="personal-finances-v2";
+const APP_SHELL=["/personal-finances/","/personal-finances/index.html","/personal-finances/manifest.json","/personal-finances/icons/icon-180.png","/personal-finances/icons/icon-512.png"];
+self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)));self.skipWaiting();});
+self.addEventListener("activate",event=>{event.waitUntil(self.clients.claim());});
 self.addEventListener("fetch",event=>{
   if(event.request.method!=="GET") return;
-  event.respondWith(
-    fetch(event.request).then(response=>{
-      const copy=response.clone();
-      caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});
-      return response;
-    }).catch(()=>caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});return r;}).catch(()=>caches.match(event.request)));
 });
